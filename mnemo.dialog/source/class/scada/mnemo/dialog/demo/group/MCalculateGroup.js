54697c4269fa06c415f2a8ffe95fd20737418ae8
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2023-09-27 00:50:56 +0300

    rabota doma

--- mnemo.dialog/source/class/scada/mnemo/dialog/demo/group/MCalculateGroup.js
+++ mnemo.dialog/source/class/scada/mnemo/dialog/demo/group/MCalculateGroup.js
@@ -7,6 +7,22 @@ qx.Mixin.define("scada.mnemo.dialog.demo.group.MCalculateGroup", {
         gridCalcGroup.setSpacing(3);
         gridCalcGroup.setColumnAlign(0, "left", "middle");
         this.сalcGroup.setLayout(gridCalcGroup);
+
+        // Добавляем текстовое поле для точности
+        const precisionLabel = new qx.ui.basic.Label("Точность:");
+        const precisionTextField = new qx.ui.form.TextField();
+        this.сalcGroup.add(precisionLabel, { row: 0, column: 0 });
+        this.сalcGroup.add(precisionTextField, { row: 0, column: 1 });
+
+        // Добавляем текстовое поле для максимального количества чисел
+        const digitCountLabel = new qx.ui.basic.Label("Макс. количество чисел:");
+        const digitCountTextField = new qx.ui.form.TextField();
+        this.сalcGroup.add(digitCountLabel, { row: 1, column: 0 });
+        this.сalcGroup.add(digitCountTextField, { row: 1, column: 1 });
+
+        // Добавляем кнопку "Применить"
+        const applyButtonCacl = new qx.ui.form.Button("Применить");
+        this.сalcGroup.add(applyButtonCacl, { row: 2, column: 0, colSpan: 2 });
     },
 
     properties: {
@@ -25,5 +41,8 @@ qx.Mixin.define("scada.mnemo.dialog.demo.group.MCalculateGroup", {
     },
 
     members: {
+        changePropCalc(){
+            
+        }
     }
 });
